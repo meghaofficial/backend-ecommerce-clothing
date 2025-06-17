cb0f5d4f1f8607f4e4468d2fe451bcb6d5f004be
meghasharma <meghaofficial789@gmail.com>
2025-06-17 22:13:41 +0530

    changed cors origin

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://your-frontend.vercel.app",
     credentials: true,
   })
 );
